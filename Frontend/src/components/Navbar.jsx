a4ca9ed2ca126f2ddc5946cfaa483ba6bfeb5275
Gourab <gourabbistu2002@gmail.com>
2024-09-14 23:51:32 +0530

    This is for github stick

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -111,7 +111,7 @@ function Navbar() {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-80 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content rounded-box z-[50] mt-3 w-52 p-2 shadow-xl dark:bg-slate-800 dark:text-white"
             >
               {navItems}
             </ul>
